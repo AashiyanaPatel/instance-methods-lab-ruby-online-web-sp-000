89153f16f9bc73ffff2f5a4c6d6fe18221883cf5
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-17 04:32:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,13 +2,16 @@ class Person
   def talk
     puts "Hello World!"
   end
-end  
+end 
+
+lisa = Person.new
+lisa.talk
+
+class Person
   def walk
     puts "The Person is walking"
   end
 end
-lisa = Person.new
-lisa.talk
 
 lisa = Person.new
 lisa.walk
